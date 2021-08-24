0bd0702af21ef411238fd8fe51949903dfc63294
Oliver Bucher <7040739+Level8Broccoli@users.noreply.github.com>
2021-08-24 20:11:48 +0200

    add port from environment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
