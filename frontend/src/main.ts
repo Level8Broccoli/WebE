f0e2533a6bacea9513be522c0db0bfe98c872e4a
Oliver <7040739+Level8Broccoli@users.noreply.github.com>
2021-10-01 13:08:36 +0200

    init vuex

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { store, key } from './store/store'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(store, key).mount('#app')
