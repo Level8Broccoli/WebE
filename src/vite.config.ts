5c2f3a8bdf34007b97cedbf94cf65533e7f81c5c
Oliver <7040739+Level8Broccoli@users.noreply.github.com>
2021-10-15 19:02:22 +0000

    remove alias, seems no longer needed

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -3,10 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "xmlhttprequest-ssl": "./node_modules/engine.io-client/lib/xmlhttprequest.js"
-    }
-  }
+  plugins: [vue()]
 })
