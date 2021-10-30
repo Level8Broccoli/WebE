36eb9db8913e8eec8058d753acc9e80e5174c3d9
Oliver <7040739+Level8Broccoli@users.noreply.github.com>
2021-10-30 18:20:21 +0000

    add lobby buttons

---
+++ src/frontend/components/lobby/LobbyButtons.vue
@@ -0,0 +1,64 @@
+<template>
+  <footer class="buttons bottom">
+    <button v-if="isCreator" @click.prevent="deleteGame" class="secondary">
+      <i class="far fa-long-arrow-left icon-left"></i>
+      {{ i18n.deleteGameButton }}
+    </button>
+    <button v-else @click.prevent="abort" class="secondary">
+      <i class="far fa-long-arrow-left icon-left"></i>
+      {{ i18n.abortButton }}
+    </button>
+    <button v-if="isCreator && gameIsValid" @click.prevent="startGame">
+      {{ i18n.startGameButton }}
+      <i class="far fa-long-arrow-right icon-right"></i>
+    </button>
+    <button v-if="isCreator && !gameIsValid" class="is-disabled" disabled>
+      {{ i18n.gameNeedsAtLeastTwoPlayers }}
+      <i class="far fa-long-arrow-right icon-right"></i>
+    </button>
+  </footer>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from "vue";
+import { useStore } from "vuex";
+import { key } from "../../store/store";
+
+export default defineComponent({
+  name: "LobbyButtons",
+  setup() {
+    const store = useStore(key);
+    const i18n = computed(() => store.getters.i18n);
+    const game = computed(() => store.state.activeGame);
+    const isCreator = computed(
+      () => game.value.creatorId === store.state.player.id
+    );
+    const gameIsValid = computed(
+      () => store.state.activeGame.players.length > 1
+    );
+
+    return {
+      i18n,
+      isCreator,
+      gameIsValid,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.secondary {
+  border: 1px solid #5432d3;
+  background-color: transparent;
+}
+.buttons {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  align-content: space-between;
+  gap: 1rem;
+}
+.is-disabled {
+  background-color: #363636;
+  cursor: not-allowed;
+}
+</style>
--- src/frontend/views/GameLobbyView.vue
+++ src/frontend/views/GameLobbyView.vue
@@ -5,7 +5,7 @@
       <hr />
       <PlayerList />
       <hr />
-      Buttons
+      <LobbyButtons />
     </div>
     <div class="right">Chat</div>
   </section>
@@ -13,14 +13,16 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import GameMeta from "../components/games/GameMeta.vue";
+import GameMeta from "../components/lobby/GameMeta.vue";
 import PlayerList from "../components/players/PlayerList.vue";
+import LobbyButtons from "../components/lobby/LobbyButtons.vue";
 
 export default defineComponent({
   name: "GameLobbyView",
   components: {
     GameMeta,
     PlayerList,
+    LobbyButtons,
   },
 });
 </script>
@@ -29,5 +31,10 @@ section {
   display: grid;
   grid-template-columns: 4fr 2fr;
   gap: 1rem;
+}
+
+.left {
+  display: grid;
+  grid-template-rows: max-content max-content 1fr max-content max-content;
 }
 </style>
