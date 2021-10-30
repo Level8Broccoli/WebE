d3ba3087dfb14c06c54bb31788a27a65fb5fe341
Oliver <7040739+Level8Broccoli@users.noreply.github.com>
2021-10-30 08:48:41 +0000

    add game rules

--- src/frontend/views/RulesView.vue
+++ src/frontend/views/RulesView.vue
@@ -1,13 +1,38 @@
 <template>
   <section>
-   <h2>Rules here!</h2>
+    <object
+      width="100%"
+      height="100%"
+      type="application/pdf"
+      :data="
+        isEnglish
+          ? '/frontend/public/game_rules_en.pdf'
+          : '/frontend/public/game_rules_de.pdf'
+      "
+    >
+      <p>{{ pdfErrorMessage }}</p>
+    </object>
   </section>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
+import { useStore } from "vuex";
+import { Language } from "../i18n/i18n";
+import { key } from "../store/store";
 
 export default defineComponent({
   name: "RulesView",
+  setup() {
+    const store = useStore(key);
+    const language = computed(() => store.state.language);
+    const isEnglish = computed(() => language.value == Language.ENGLISH);
+    const i18n = computed(() => store.getters.i18n);
+    const pdfErrorMessage = computed(() => i18n.value.pdfErrorMessage);
+    return {
+      isEnglish,
+      pdfErrorMessage,
+    };
+  },
 });
 </script>
